ac26e84b1423b5318d67986c69c114fe9ca05fea
smilekison <smilekishan.sk@gmail.com>
2020-07-07 23:07:43 +0545

    6th commit
    Admin part (listing and Realtor finish with css)

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Realtor
 
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email')
+    list_per_page = 20
+
 # Register your models here.
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
 # Register your models here.
