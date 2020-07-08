a14b908dc4aa7126c2626db969863c9b8b0d3f24
smilekison <smilekishan.sk@gmail.com>
2020-07-08 12:50:31 +0545

    7. View Methods, Display & Search
    All the functionalities are done

---
+++ listings/choices.py
@@ -0,0 +1,43 @@
+bedroom_choices = {
+	'1':1,
+	'2':2,
+	'3':3,
+	'4':4,
+	'5':5,
+	'6':6,
+	'7':7,
+	'8':8,
+	'9':9,
+	'10':10
+}
+
+price_choices ={
+	'100000': '$100,000',
+	'200000': '$200,000',
+	'300000': '$300,000',
+	'400000': '$400,000',
+	'500000': '$500,000',
+	'600000': '$600,000',
+	'700000': '$700,000',
+	'800000': '$800,000',
+	'900000': '$900,000',
+	'1000000': '$1,000,000'
+}
+
+state_choices = {
+	'BAG':'Bagmati',
+	'BHE':'Bheri',
+	'DHA':'Dhawalagiri',
+	'GAN':'Gandaki',
+	'JAN':'Janakpur',
+	'KAR':'Karnali',
+	'KOS':'Koshi',
+	'LUM':'Lumbini',
+	'MKL':'Mahakali',
+	'MEC':'Mechi',
+	'NAR':'Narayani',
+	'RAP':'Rapti',
+	'SGR':'Sagarmatha',
+	'SETI':'Seti'
+
+}
