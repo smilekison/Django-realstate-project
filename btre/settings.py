0c2a69a61e62db2d7d9d51bdfa162d59ce9072e5
smilekison <smilekishan.sk@gmail.com>
2020-07-08 16:47:33 +0545

    8. Accounts & Authentication
    8. Accounts & Authentication

--- btre/settings.py
+++ btre/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -135,3 +136,9 @@ STATICFILES_DIRS = [
 #Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
